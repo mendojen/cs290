ab912c6d53cd085e64b001eb7cc42aab064e51cd
mendojen <mendojen@onid.oregonstate.edu>
2015-11-15 16:46:07 -0800

    Update formdemo.js

--- formdemo.js
+++ formdemo.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
