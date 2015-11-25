fc631d0967c30125a03ca41068acb591fbe0bb38
mendojen <mendojen@onid.oregonstate.edu>
2015-11-25 12:01:59 -0800

    Update final.js

--- final.js
+++ final.js
@@ -34,18 +34,21 @@ app.get('/insert',function(req,res,next){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    var createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
+  [your connection pool].query("DROP TABLE IF EXISTS todo", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
     mysql.pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('home',context);
     })
   });
 });
+
 app.get('/simple-update',function(req,res,next){
   var context = {};
   mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
