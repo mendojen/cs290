89e5ed2ddb62cabef48e711f9c5d346e6daa2bec
mendojen <mendojen@onid.oregonstate.edu>
2015-11-25 11:20:29 -0800

    Update final.js

--- final.js
+++ final.js
@@ -81,3 +81,19 @@ app.get('/safe-update',function(req,res,next){
     }
   });
 });
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
