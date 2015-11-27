e00eace0f23e8dbec5393452d38c0f240e46c251
mendojen <mendojen@onid.oregonstate.edu>
2015-11-26 18:15:48 -0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -22,7 +22,7 @@ app.get('/summonerid', function(req, res, next){
     if(!err && response.statusCode < 400){
       context.riot = JSON.parse(body);
       res.render('userid',context);
-      console.log(riot.id);
+      console.log(body.id);
     } else {
       if(response){
         console.log(response.statusCode);
