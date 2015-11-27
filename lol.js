b85825ce7da4677ba8bc8ce3bfb0befe1eaa0b95
mendojen <mendojen@onid.oregonstate.edu>
2015-11-26 16:27:46 -0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 app.get('/', function (req,res){
    var context = {};
-  res.render('form', context)
+  res.render('homepage', context)
 });
 
 app.get('/zip',function(req,res,next){
