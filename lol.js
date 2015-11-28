2058f6d40cfb160b15c475889e904de554ba0f73
mendojen <mendojen@onid.oregonstate.edu>
2015-11-28 00:59:09 -0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -20,7 +20,7 @@ app.get('/recentgames', function(req, res, next){
   var context = {};
   request('https://na.api.pvp.net/api/lol/' + req.query.region + '/v1.3/game/by-summoner/'+req.query.userid+'/recent?api_key=' + credentials.riotKey, function(err, response, body){
     if(!err && response.statusCode < 400){
-      context.riot = JSON.stringify(body);
+      context.riot = JSON.parse(body);
        console.log(context.riot.games[0].gameMode);
       res.render('recent',context);
       
