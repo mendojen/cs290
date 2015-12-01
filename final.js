d7cf2b1028828f722336e27e8108d2efb57f633d
mendojen <mendojen@onid.oregonstate.edu>
2015-12-01 15:34:13 -0800

    Update final.js

--- final.js
+++ final.js
@@ -41,7 +41,7 @@ app.get('/insert',function(req,res,next){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.query("DROP TABLE IF EXISTS todo", function(err){ //replace your connection pool with the your variable containing the connection pool
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
