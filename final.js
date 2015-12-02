46ddde42c497f9203bb95a366071641fcdb60302
mendojen <mendojen@onid.oregonstate.edu>
2015-12-02 13:53:44 -0800

    Update final.js

--- final.js
+++ final.js
@@ -26,7 +26,7 @@ app.get('/',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO workout (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name],[req.query.reps],[req.query.weight],[req.query.date],[req.query.pounds], function(err, result){
+  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name],[req.query.reps],[req.query.weight],[req.query.date],[req.query.pounds], function(err, result){
     if(err){
       next(err);
       return;
