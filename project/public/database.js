4d5a92a5b70e05bb91267c8acbbe5f26c9f8933f
mendojen <mendojen@onid.oregonstate.edu>
2015-12-03 22:27:43 -0800

    Update database.js

--- project/public/database.js
+++ project/public/database.js
@@ -54,21 +54,14 @@
 			  cell.style.textAlign = "right";
 			row.appendChild(cell);
 			cell.style.border= "thin solid black";
-		  
 		  });
 		  table.appendChild(row);
-			
 		}); 
 	}
-	
     return table;
- 
  }
 
-
-
  function loaded(event){
-	    //bindButtons();
 		var req = new XMLHttpRequest();
 		
 		req.open("GET", 'http://52.25.198.76:3000/select', true);
@@ -79,21 +72,16 @@
 				document.body.appendChild(buildTable(response));
 				}
 			});
-		req.send();
-		
+		req.send();		
  }
   
    function reloaded(event){
 		var table = document.getElementById("myTable");
-/*		while(table.rows.length) {
-			table.deleteRow(0);
-		}*/
 		var req = new XMLHttpRequest();
 		
 		req.open("GET", 'http://52.25.198.76:3000/select', true);
 		req.addEventListener('load', function(){
 			if (req.status >= 200 && req.status < 400){
-				table.parentNode.removeChild(table);
 				var response = JSON.parse(req.responseText);			
 				document.body.appendChild(buildTable(response));
 				}
@@ -101,22 +89,7 @@
 		req.send();
  }
  
-document.addEventListener('DOMContentLoaded', bindButtons);
-//Depending on which button is pushed, that specific event function is called
-
-
- function cleartable(pick_table){
-	var table = pick_table;
-    while(table.rows.length) {
-	table.deleteRow(0);
-	}
- }
- 
-function bindButtons()
- {
-	 
-	var myhandle = function(event)
-	{
+function add_button(){
 		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var wname = document.getElementById('wname').value ;
@@ -130,7 +103,9 @@ function bindButtons()
 		
 		req.onreadystatechange=function() {
 		if (req.readyState == 4 && req.status == 200) {
-			  addrow(wname,reps,weight,date,pounds);
+//			  addrow(wname,reps,weight,date,pounds);
+			table.parentNode.removeChild(table);
+			reloaded();
 			}
 		  };
 		
@@ -138,120 +113,130 @@ function bindButtons()
 		event.preventDefault(); 
 		} else {
 			alert("Not added. Name input missing");
-		}	
-      		
-	};
-    document.getElementById('add').addEventListener('click', myhandle);
-    
+		}
+}	
 	
-	
-	document.getElementById('delete').addEventListener('click', function(event)
+function delete_button()
 	{
-		
+		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var deleteID = document.getElementById('delete_id').value ;
 		
 		
 		req.open("GET", 'http://52.25.198.76:3000/delete?delete_id=' +deleteID, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
-				reloaded();
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
+		  };
+		 
 		req.send(null);
-		event.preventDefault();  	
-	});
-	document.getElementById('uname').addEventListener('click', function(event)
+		event.preventDefault(); 
+		table.parentNode.removeChild(table);
+		reloaded();
+}
+function updatename()
 	{
+		var table = document.getElementById("myTable");
 		
 		var req = new XMLHttpRequest();
 		var new_name = document.getElementById('ename').value ;
 		var selected_id=document.getElementById('eid').value;
 		if (selected_id!=""){
 		req.open("GET", 'http://52.25.198.76:3000/updatename?ename=' + new_name + '&eid='+selected_id, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
+		  }
+		};
+		 
 		req.send(null);
-		event.preventDefault();
-		}		
-	});
-	document.getElementById('ureps').addEventListener('click', function(event)
+		event.preventDefault(); 
+		table.parentNode.removeChild(table);
+		reloaded();
+}
+function updatereps()
 	{
-		
+		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var new_rep = document.getElementById('ereps').value ;
 		var selected_id=document.getElementById('eid').value;
 		if (selected_id!=""){
 		req.open("GET", 'http://52.25.198.76:3000/updatereps?ereps=' + new_rep + '&eid='+selected_id, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
+		  }
+		};
+		 
 		req.send(null);
 		event.preventDefault(); 
-		}		
-	});
-	document.getElementById('uweight').addEventListener('click', function(event)
+		table.parentNode.removeChild(table);
+		reloaded();
+}
+function updateweight()
 	{
-		
+		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var new_weight = document.getElementById('eweight').value ;
 		var selected_id=document.getElementById('eid').value;
 		if (selected_id!=""){
 		req.open("GET", 'http://52.25.198.76:3000/updateweight?eweight=' + new_weight + '&eid='+selected_id, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
+		  }
+		};
+		 
 		req.send(null);
-		event.preventDefault();  
-		}		
-	});
-	document.getElementById('udate').addEventListener('click', function(event)
+		event.preventDefault(); 
+		table.parentNode.removeChild(table);
+		reloaded();
+}
+function updatedate()
 	{
-		
+		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var new_date = document.getElementById('edate').value ;
 		var selected_id=document.getElementById('eid').value;
 		if (selected_id!=""){
 		req.open("GET", 'http://52.25.198.76:3000/updatedate?edate=' + new_date + '&eid='+selected_id, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
+		  }
+		};
+		 
 		req.send(null);
-		event.preventDefault();  
-		}
-	});
-	document.getElementById('upounds').addEventListener('click', function(event)
+		event.preventDefault(); 
+		table.parentNode.removeChild(table);
+		reloaded();
+}
+function updatepounds()
 	{
+		var table = document.getElementById("myTable");
 		var req = new XMLHttpRequest();
 		var new_pounds = document.getElementById('epounds').value ;
 		var selected_id=document.getElementById('eid').value;
 		if (selected_id!=""){
 		req.open("GET", 'http://52.25.198.76:3000/updatepounds?epounds=' + new_pounds + '&eid='+selected_id, true);
-		req.addEventListener('load', function(){
-			if (req.status >= 200 && req.status <400){
+		req.onreadystatechange=function() {
+		if (req.readyState == 4 && req.status == 200) {
 			}
-		});
-		
+		  }
+		};
+		 
 		req.send(null);
-		event.preventDefault();  	
-		}
-	});
-	
- }
+		event.preventDefault(); 
+		table.parentNode.removeChild(table);
+		reloaded();
+}
 
- 
- 
- function addrow(wname, reps, weight, date, pounds){
+ function addrow(id,wname, reps, weight, date, pounds){
 	var table = document.getElementById("myTable");
 	var row1 = table.insertRow(table.rows.length);
 	
 	var cell1 = row1.insertCell(0);
-	cell1.textContent = table.rows.length;
+	cell1.textContent = id;
 	cell1.style.border= "thin solid black";
 	
 	var cell1 = row1.insertCell(1);
